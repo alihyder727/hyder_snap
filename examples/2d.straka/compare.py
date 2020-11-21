3bc264da20d4bdd812c7905f60e1683b46f48897
cli/asgard <lumos.leee@gmail.com>
2020-11-21 05:50:49 -0800

    mpibuild #10

--- examples/2d.straka/compare.py
+++ examples/2d.straka/compare.py
@@ -7,14 +7,14 @@ case1 = sys.argv[1]
 case2 = sys.argv[2]
 
 data1 = Dataset(case1, 'r')
-theta1 = data1['theta'][-1,0,:,:]
+theta1 = data1['theta'][1,0,:,:]
 
 data2 = Dataset(case2, 'r')
-theta2 = data2['theta'][-1,0,:,:]
+theta2 = data2['theta'][1,0,:,:]
 
 diff = sqrt(sum((theta2 - theta1)*(theta2 - theta1)))
 
-if diff < 1.E-8:
+if diff < 1.E-6:
   print('2D straka test passed.')
 else:
   raise ValueError('ERROR: 2D straka test failed. L2-norm is %.2g' % diff)
