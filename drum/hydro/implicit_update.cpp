df3209098dbbf8e4acb586d9c42e3eef028ca068
cli/asgard <lumos.leee@gmail.com>
2021-04-14 06:36:17 -0700

    archive one other file

---
+++ drum/hydro/implicit_update.cpp
@@ -0,0 +1,34 @@
+//! \file implicit_update.cpp
+//  \brief final step of full implicit solver
+
+// Athena++ headers
+#include "../mesh/mesh.hpp"
+#include "hydro.hpp"
+#include "implicit/implicit_solver.hpp"
+
+void Hydro::ImplicitUpdate(AthenaArray<Real> &du)
+{
+  MeshBlock *pmb = pmy_block;
+  int is = pmb->is; int js = pmb->js; int ks = pmb->ks;
+  int ie = pmb->ie; int je = pmb->je; int ke = pmb->ke;
+
+  if (implicit_flag == 2) {
+    for (int k = ks; k <= ke; ++k)
+      for (int j = js; j <= je; ++j)
+        for (int i = is; i <= ie; ++i) {
+          du(IDN,k,j,i) = pimp1->du_(IDN,k,j,i);
+          du(IVX,k,j,i) = pimp1->du_(IVX,k,j,i);
+          du(IEN,k,j,i) = pimp1->du_(IEN,k,j,i);
+        }
+  } else if (implicit_flag == 3) {
+    for (int k = ks; k <= ke; ++k)
+      for (int j = js; j <= je; ++j)
+        for (int i = is; i <= ie; ++i) {
+          du(IDN,k,j,i) = pimp1->du_(IDN,k,j,i) + pimp2->du_(IDN,i,k,j) + pimp3->du_(IDN,j,i,k);
+          du(IVX,k,j,i) = pimp1->du_(IVX,k,j,i);
+          du(IVY,k,j,i) = pimp2->du_(IVY,k,j,i);
+          du(IVZ,k,j,i) = pimp3->du_(IVZ,k,j,i);
+          du(IEN,k,j,i) = pimp1->du_(IEN,k,j,i) + pimp2->du_(IEN,i,k,j) + pimp3->du_(IEN,j,i,k);
+        }
+  }
+}
