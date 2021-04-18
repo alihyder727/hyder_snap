8664aaeff27a601de6cf08d71eb6494de489f859
cli/asgard <lumos.leee@gmail.com>
2021-04-18 08:52:16 -0700

    finish 3d implicit

--- drum/pgen/robert.cpp
+++ drum/pgen/robert.cpp
@@ -28,18 +28,19 @@ Real p0;
 void MeshBlock::InitUserMeshBlockData(ParameterInput *pin)
 {
   AllocateUserOutputVariables(2);
-  SetUserOutputVariableName(0, "temp");
-  SetUserOutputVariableName(1, "theta");
+  SetUserOutputVariableName(0, "temp", "temperature", "K");
+  SetUserOutputVariableName(1, "theta", "potential temperature", "K");
 }
 
 void MeshBlock::UserWorkBeforeOutput(ParameterInput *pin)
 {
   Real gamma = peos->GetGamma();
-  for (int j = js; j <= je; ++j)
-    for (int i = is; i <= ie; ++i) {
-      user_out_var(0,j,i) = pthermo->Temp(phydro->w.at(j,i));
-      user_out_var(1,j,i) = pthermo->Theta(phydro->w.at(j,i), p0);
-    }
+  for (int k = ks; k <= ke; ++k)
+    for (int j = js; j <= je; ++j)
+      for (int i = is; i <= ie; ++i) {
+        user_out_var(0,k,j,i) = pthermo->Temp(phydro->w.at(k,j,i));
+        user_out_var(1,k,j,i) = pthermo->Theta(phydro->w.at(k,j,i), p0);
+      }
 }
 
 void Mesh::InitUserMeshData(ParameterInput *pin)
@@ -56,6 +57,7 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin)
   Real cp = gamma/(gamma - 1.)*Rd;
 
   Real xc = pin->GetReal("problem", "xc");
+  Real yc = pin->GetOrAddReal("problem", "yc", 0.);
   Real zc = pin->GetReal("problem", "zc");
   Real s = pin->GetReal("problem", "s");
   Real a = pin->GetReal("problem", "a");
@@ -63,30 +65,37 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin)
 
   bool uniform_bubble = pin->GetOrAddBoolean("problem", "uniform_bubble", false);
 
-  for (int j = js; j <= je; ++j) {
-    for (int i = is; i <= ie; ++i) {
-      Real x1 = pcoord->x1v(i);
-      Real x2 = pcoord->x2v(j);
-      Real r = sqrt((x2-xc)*(x2-xc) + (x1-zc)*(x1-zc));
-      Real temp = Ts - grav*x1/cp;
-      phydro->w(IPR,j,i) = p0*pow(temp/Ts, cp/Rd);
-      if (r <= a)
-        temp += dT*pow(phydro->w(IPR,j,i)/p0, Rd/cp);
-      else if (!uniform_bubble)
-        temp += dT*exp(-(r-a)*(r-a)/(s*s))*pow(phydro->w(IPR,j,i)/p0, Rd/cp);
-      phydro->w(IDN,j,i) = phydro->w(IPR,j,i)/(Rd*temp);
-      phydro->w(IVX,j,i) = phydro->w(IVY,j,i) = 0.;
-    }
-  }
-
-  if (pbval->block_bcs[inner_x1] == BoundaryFlag::outflow) {
-    for (int j = js-1; j <= je+1; ++j)
-      for (int i = 1; i <= NGHOST; ++i) {
-        Real temp = Ts - grav*pcoord->x1f(is)/cp;
-        phydro->w(IPR,j,is-i) = p0*pow(temp/Ts, cp/Rd);
-        phydro->w(IDN,j,is-i) = phydro->w(IPR,j,is-i)/(Rd*temp);
-        phydro->w(IVX,j,is-i) = phydro->w(IVY,j,is-i) = 0.;
+  for (int k = ks; k <= ke; ++k)
+    for (int j = js; j <= je; ++j)
+      for (int i = is; i <= ie; ++i) {
+        Real x1 = pcoord->x1v(i);
+        Real x2 = pcoord->x2v(j);
+        Real x3 = pcoord->x3v(k);
+        Real r = sqrt((x3-yc)*(x3-yc) + (x2-xc)*(x2-xc) + (x1-zc)*(x1-zc));
+        Real temp = Ts - grav*x1/cp;
+        phydro->w(IPR,k,j,i) = p0*pow(temp/Ts, cp/Rd);
+        if (r <= a)
+          temp += dT*pow(phydro->w(IPR,k,j,i)/p0, Rd/cp);
+        else if (!uniform_bubble)
+          temp += dT*exp(-(r-a)*(r-a)/(s*s))*pow(phydro->w(IPR,k,j,i)/p0, Rd/cp);
+        phydro->w(IDN,k,j,i) = phydro->w(IPR,k,j,i)/(Rd*temp);
+        phydro->w(IVX,k,j,i) = phydro->w(IVY,k,j,i) = 0.;
       }
+
+  // setup open lower boundary
+  int kl = block_size.nx3 == 1 ? ks : ks-NGHOST;
+  int ku = block_size.nx3 == 1 ? ke : ke+NGHOST;
+  int jl = block_size.nx2 == 1 ? js : js-NGHOST;
+  int ju = block_size.nx2 == 1 ? je : je+NGHOST;
+  if (pbval->block_bcs[inner_x1] == BoundaryFlag::outflow) {
+    for (int k = kl; k <= ku; ++k)
+      for (int j = jl; j <= ju; ++j)
+        for (int i = 1; i <= NGHOST; ++i) {
+          Real temp = Ts - grav*pcoord->x1f(is)/cp;
+          phydro->w(IPR,k,j,is-i) = p0*pow(temp/Ts, cp/Rd);
+          phydro->w(IDN,k,j,is-i) = phydro->w(IPR,k,j,is-i)/(Rd*temp);
+          phydro->w(IVX,k,j,is-i) = phydro->w(IVY,k,j,is-i) = 0.;
+        }
   }
 
   peos->PrimitiveToConserved(phydro->w, pfield->bcc, phydro->u, pcoord, is, ie, js, je, ks, ke);
