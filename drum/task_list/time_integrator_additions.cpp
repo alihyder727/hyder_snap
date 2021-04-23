8a13e9ecb2243f69763519cbf640ee7c64d14352
cli/asgard <lumos.leee@gmail.com>
2021-04-23 16:47:52 -0700

    x3->x2->x1, xiz model works

--- drum/task_list/time_integrator_additions.cpp
+++ drum/task_list/time_integrator_additions.cpp
@@ -16,12 +16,12 @@ enum TaskStatus TimeIntegratorTaskList::UpdateHydro(MeshBlock *pmb, int stage) {
   Real dt = pmb->pmy_mesh->dt;
 
   // do implicit coorection at every stage
-  // X1DIR
-  if (ph->implicit_flag & 1)
+  // X3DIR
+  if ((ph->implicit_flag & (1<<2)) && (pmb->ncells3 > 1))
     if (ph->implicit_flag & (1<<3))
-      ph->pimp1->FullCorrection(ph->du, ph->w, stage_wghts[stage-1].beta*dt);
+      ph->pimp3->FullCorrection(ph->du, ph->w, stage_wghts[stage-1].beta*dt);
     else
-      ph->pimp1->PartialCorrection(ph->du, ph->w, stage_wghts[stage-1].beta*dt);
+      ph->pimp3->PartialCorrection(ph->du, ph->w, stage_wghts[stage-1].beta*dt);
 
   // X2DIR
   if ((ph->implicit_flag & (1<<1)) && (pmb->ncells2 > 1))
@@ -30,12 +30,12 @@ enum TaskStatus TimeIntegratorTaskList::UpdateHydro(MeshBlock *pmb, int stage) {
     else
       ph->pimp2->PartialCorrection(ph->du, ph->w, stage_wghts[stage-1].beta*dt);
 
-  // X3DIR
-  if ((ph->implicit_flag & (1<<2)) && (pmb->ncells3 > 1))
+  // X1DIR
+  if (ph->implicit_flag & 1)
     if (ph->implicit_flag & (1<<3))
-      ph->pimp3->FullCorrection(ph->du, ph->w, stage_wghts[stage-1].beta*dt);
+      ph->pimp1->FullCorrection(ph->du, ph->w, stage_wghts[stage-1].beta*dt);
     else
-      ph->pimp3->PartialCorrection(ph->du, ph->w, stage_wghts[stage-1].beta*dt);
+      ph->pimp1->PartialCorrection(ph->du, ph->w, stage_wghts[stage-1].beta*dt);
 
   Real wghts[3];
   wghts[0] = 1.;
