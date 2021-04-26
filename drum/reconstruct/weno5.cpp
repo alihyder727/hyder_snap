042294c08281d4f194d8fbc5346a296aae84821d
cli/asgard <lumos.leee@gmail.com>
2021-04-26 14:48:24 -0700

    clean up weno

--- drum/reconstruct/weno5.cpp
+++ drum/reconstruct/weno5.cpp
@@ -26,80 +26,56 @@ void Reconstruction::Weno5X1(const int k, const int j, const int il, const int i
   AthenaArray<Real> &wl, AthenaArray<Real> &wr)
 {
   MeshBlock *pmb = pmy_block_;
-  for (int n=0; n<=NVAPOR; ++n) 
+  Real v[5], scale;
+  for (int n=0; n<NMASS; ++n) {
+    scale = 1.E-16;
 #pragma omp simd
+    for (int m = -2; m <= 2; ++m) scale += w(n,k,j,il+m);
     for (int i=il; i<=iu; ++i) {
-      Real scale = 0.;
-      for (int m = -2; m <= 2; ++m) scale += (w(n,k,j,i+m) + 1.E-16)/5.;
-      wl(n,i+1) = interp_weno5(w(n,k,j,i+2)/scale, w(n,k,j,i+1)/scale, w(n,k,j,i)/scale,
-                               w(n,k,j,i-1)/scale, w(n,k,j,i-2)/scale);
-      wl(n,i+1) *= scale;
-      wr(n,i) = interp_weno5(w(n,k,j,i-2)/scale, w(n,k,j,i-1)/scale, w(n,k,j,i)/scale,
-                             w(n,k,j,i+1)/scale, w(n,k,j,i+2)/scale);
-      wr(n,i) *= scale;
-    }
-
-  for (int n=NVAPOR+1; n<NMASS; ++n) 
 #pragma omp simd
-    for (int i=il; i<=iu; ++i) {
-      Real scale = 0.;
-      for (int m = -1; m <= 1; ++m) scale += (w(n,k,j,i+m) + 1.E-16)/3.;
-      wl(n,i+1) = interp_weno3(w(n,k,j,i+1)/scale, w(n,k,j,i)/scale, w(n,k,j,i-1)/scale);
-      wl(n,i+1) *= scale;
-
-      wr(n,i) = interp_weno3(w(n,k,j,i-1)/scale, w(n,k,j,i)/scale, w(n,k,j,i+1)/scale);
-      wr(n,i) *= scale;
+      for (int m = -2; m <= 2; ++m) v[m+2] = w(n,k,j,i+m)/(5.*scale);
+      wl(n,i+1) = interp_weno5(v[4], v[3], v[2], v[1], v[0])*5.*scale;
+      wr(n,i  ) = interp_weno5(v[0], v[1], v[2], v[3], v[4])*5.*scale;
+      scale += w(n,k,j,i+3) - w(n,k,j,i-2);
     }
+  }
+
+  int ng1 = 0, ng2 = 0;
+  if (pmb->pbval->block_bcs[inner_x1] != BoundaryFlag::block)
+    ng1 = NGHOST;
+  if (pmb->pbval->block_bcs[outer_x1] != BoundaryFlag::block)
+    ng2 = NGHOST;
 
   for (int n=NMASS; n<NHYDRO; ++n) {
-    if (pmb->pbval->block_bcs[inner_x1] != BoundaryFlag::block)
-      for (int i=il; i<il+NGHOST; ++i) {
-        Real scale = 0.;
-        for (int m = -2; m <= 2; ++m) scale += (w(n,k,j,i+m) + 1.E-16)/5.;
-        wl(n,i+1) = interp_weno5(w(n,k,j,i+2)/scale, w(n,k,j,i+1)/scale, w(n,k,j,i)/scale,
-                                 w(n,k,j,i-1)/scale, w(n,k,j,i-2)/scale);
-        wl(n,i+1) *= scale;
-        wr(n,i  ) = interp_weno5(w(n,k,j,i-2)/scale, w(n,k,j,i-1)/scale, w(n,k,j,i)/scale,
-                                 w(n,k,j,i+1)/scale, w(n,k,j,i+2)/scale);
-        wr(n,i  ) *= scale;
-      }
-    else
-      for (int i=il; i<il+NGHOST; ++i) {
-        wl(n,i+1) = interp_cp5(w(n,k,j,i+2),w(n,k,j,i+1),w(n,k,j,i),w(n,k,j,i-1),w(n,k,j,i-2));
-        wr(n,i  ) = interp_cp5(w(n,k,j,i-2),w(n,k,j,i-1),w(n,k,j,i),w(n,k,j,i+1),w(n,k,j,i+2));
-      }
-
-    //if (n == IVX || n == IPR)
+    // left boundary
 #pragma omp simd
-      for (int i=il+NGHOST; i<=iu-NGHOST; ++i) {
-        wl(n,i+1) = interp_cp5(w(n,k,j,i+2),w(n,k,j,i+1),w(n,k,j,i),w(n,k,j,i-1),w(n,k,j,i-2));
-        wr(n,i  ) = interp_cp5(w(n,k,j,i-2),w(n,k,j,i-1),w(n,k,j,i),w(n,k,j,i+1),w(n,k,j,i+2));
-      }
-    /*else
+    scale = 1.E-16;
+    for (int m = -2; m <= 2; ++m) scale += w(n,k,j,il+m);
+    for (int i=il; i<il+ng1; ++i) {
 #pragma omp simd
-      for (int i=il+NGHOST; i<=iu-NGHOST; ++i) {
-        wl(n,i+1) = interp_bp5(w(n,k,j,i+3),w(n,k,j,i+2),w(n,k,j,i+1),
-                               w(n,k,j,i),w(n,k,j,i-1),w(n,k,j,i-2),-sign(w(IVX,k,j,i)));
-        wr(n,i  ) = interp_bp5(w(n,k,j,i-3),w(n,k,j,i-2),w(n,k,j,i-1),
-                               w(n,k,j,i),w(n,k,j,i+1),w(n,k,j,i+2), sign(w(IVX,k,j,i)));
-      }*/
+      for (int m = -2; m <= 2; ++m) v[m+2] = w(n,k,j,i+m)/(5.*scale);
+      wl(n,i+1) = interp_weno5(v[4], v[3], v[2], v[1], v[0])*5.*scale;
+      wr(n,i  ) = interp_weno5(v[0], v[1], v[2], v[3], v[4])*5.*scale;
+      scale += w(n,k,j,i+3) - w(n,k,j,i-2);
+    }
 
-    if (pmb->pbval->block_bcs[outer_x1] == BoundaryFlag::block)
-      for (int i=iu-NGHOST+1; i<=iu; ++i) {
-        wl(n,i+1) = interp_cp5(w(n,k,j,i+2),w(n,k,j,i+1),w(n,k,j,i),w(n,k,j,i-1),w(n,k,j,i-2));
-        wr(n,i  ) = interp_cp5(w(n,k,j,i-2),w(n,k,j,i-1),w(n,k,j,i),w(n,k,j,i+1),w(n,k,j,i+2));
-      }
-    else
-      for (int i=iu-NGHOST+1; i<=iu; ++i) {
-        Real scale = 0.;
-        for (int m = -2; m <= 2; ++m) scale += (w(n,k,j,i+m) + 1.E-16)/5.;
-        wl(n,i+1) = interp_weno5(w(n,k,j,i+2)/scale, w(n,k,j,i+1)/scale, w(n,k,j,i)/scale,
-                                 w(n,k,j,i-1)/scale, w(n,k,j,i-2)/scale);
-        wl(n,i+1) *= scale;
-        wr(n,i  ) = interp_weno5(w(n,k,j,i-2)/scale, w(n,k,j,i-1)/scale, w(n,k,j,i)/scale,
-                                 w(n,k,j,i+1)/scale, w(n,k,j,i+2)/scale);
-        wr(n,i  ) *= scale;
-      }
+    // interior
+    for (int i=il+ng1; i<=iu-ng2; ++i) {
+      wl(n,i+1) = interp_cp5(w(n,k,j,i+2),w(n,k,j,i+1),w(n,k,j,i),w(n,k,j,i-1),w(n,k,j,i-2));
+      wr(n,i  ) = interp_cp5(w(n,k,j,i-2),w(n,k,j,i-1),w(n,k,j,i),w(n,k,j,i+1),w(n,k,j,i+2));
+    }
+
+    // right boundary
+    scale = 1.E-16;
+#pragma omp simd
+    for (int m = -2; m <= 2; ++m) scale += w(n,k,j,iu-ng2+1+m);
+    for (int i=iu-ng2+1; i<=iu; ++i) {
+#pragma omp simd
+      for (int m = -2; m <= 2; ++m) v[m+2] = w(n,k,j,i+m)/(5.*scale);
+      wl(n,i+1) = interp_weno5(v[4], v[3], v[2], v[1], v[0])*5.*scale;
+      wr(n,i  ) = interp_weno5(v[0], v[1], v[2], v[3], v[4])*5.*scale;
+      scale += w(n,k,j,i+3) - w(n,k,j,i-2);
+    }
   }
 
   return;
@@ -113,45 +89,52 @@ void Reconstruction::Weno5X2(const int k, const int j, const int il, const int i
   const AthenaArray<Real> &w, const AthenaArray<Real> &bcc,
   AthenaArray<Real> &wl, AthenaArray<Real> &wr)
 {
-  for (int n=0; n<=NVAPOR; ++n)
+  MeshBlock *pmb = pmy_block_;
+  Real v[5], scale;
+  for (int n=0; n<NMASS; ++n) {
+    scale = 1.E-16;
 #pragma omp simd
+    for (int m = -2; m <= 2; ++m) scale += w(n,k,j,il+m);
     for (int i=il; i<=iu; ++i) {
-      Real scale = 0.;
-      for (int m = -2; m <= 2; ++m) scale += (w(n,k,j+m,i) + 1.E-16)/5.;
-      wl(n,i) = interp_weno5(w(n,k,j+2,i)/scale, w(n,k,j+1,i)/scale, w(n,k,j,i)/scale,
-                             w(n,k,j-1,i)/scale, w(n,k,j-2,i)/scale);
-      wl(n,i) *= scale;
-      wr(n,i) = interp_weno5(w(n,k,j-2,i)/scale, w(n,k,j-1,i)/scale, w(n,k,j,i)/scale,
-                             w(n,k,j+1,i)/scale, w(n,k,j+2,i)/scale);
-      wr(n,i) *= scale;
-    }
-
-  for (int n=NVAPOR+1; n<NMASS; ++n) 
 #pragma omp simd
-    for (int i=il; i<=iu; ++i) {
-      Real scale = 0.;
-      for (int m = -1; m <= 1; ++m) scale += (w(n,k,j+m,i) + 1.E-16)/3.;
-      wl(n,i) = interp_weno3(w(n,k,j+1,i)/scale, w(n,k,j,i)/scale, w(n,k,j-1,i)/scale);
-      wl(n,i) *= scale;
-      wr(n,i) = interp_weno3(w(n,k,j-1,i)/scale, w(n,k,j,i)/scale, w(n,k,j+1,i)/scale);
-      wr(n,i) *= scale;
+      for (int m = -2; m <= 2; ++m) v[m+2] = w(n,k,j+m,i)/(5.*scale);
+      // wl(j+1/2)
+      wl(n,i) = interp_weno5(v[4], v[3], v[2], v[1], v[0])*5.*scale;
+      // wr(j-1/2)
+      wr(n,i) = interp_weno5(v[0], v[1], v[2], v[3], v[4])*5.*scale;
+      scale += w(n,k,j,i+3) - w(n,k,j,i-2);
     }
+  }
 
-  for (int n=NMASS; n<NHYDRO; ++n) {
-    //if (n == IVY || n == IPR)
+  int ng1 = 0, ng2 = 0;
+  if (pmb->pbval->block_bcs[inner_x2] != BoundaryFlag::block)
+    ng1 = NGHOST;
+  if (pmb->pbval->block_bcs[outer_x2] != BoundaryFlag::block)
+    ng2 = NGHOST;
+
+  if ((j < pmb->js-1 + ng1) || (j > pmb->je+1 - ng2)) {
+    for (int n=NMASS; n<NHYDRO; ++n) {
+      scale = 1.E-16;
+#pragma omp simd
+      for (int m = -2; m <= 2; ++m) scale += w(n,k,j,il+m);
+      for (int i=il; i<=iu; ++i) {
+#pragma omp simd
+        for (int m = -2; m <= 2; ++m) v[m+2] = w(n,k,j+m,i)/(5.*scale);
+        // wl(j+1/2)
+        wl(n,i) = interp_weno5(v[4], v[3], v[2], v[1], v[0])*5.*scale;
+        // wr(j-1/2)
+        wr(n,i) = interp_weno5(v[0], v[1], v[2], v[3], v[4])*5.*scale;
+        scale += w(n,k,j,i+3) - w(n,k,j,i-2);
+      }
+    }
+  } else {
+    for (int n=NMASS; n<NHYDRO; ++n) {
 #pragma omp simd
       for (int i=il; i<=iu; ++i) {
         wl(n,i) = interp_cp5(w(n,k,j+2,i),w(n,k,j+1,i),w(n,k,j,i),w(n,k,j-1,i),w(n,k,j-2,i));
         wr(n,i) = interp_cp5(w(n,k,j-2,i),w(n,k,j-1,i),w(n,k,j,i),w(n,k,j+1,i),w(n,k,j+2,i));
       }
-    /*else 
-#pragma omp simd
-      for (int i=il; i<=iu; ++i) {
-        wl(n,i) = interp_bp5(w(n,k,j+3,i),w(n,k,j+2,i),w(n,k,j+1,i),
-                             w(n,k,j,i),w(n,k,j-1,i),w(n,k,j-2,i),-sign(w(IVY,k,j,i)));
-        wr(n,i) = interp_bp5(w(n,k,j-3,i),w(n,k,j-2,i),w(n,k,j-1,i),
-                             w(n,k,j,i),w(n,k,j+1,i),w(n,k,j+2,i), sign(w(IVY,k,j,i)));
-      }*/
+    }
   }
 
   return;
@@ -165,45 +148,52 @@ void Reconstruction::Weno5X3(const int k, const int j, const int il, const int i
   const AthenaArray<Real> &w, const AthenaArray<Real> &bcc,
   AthenaArray<Real> &wl, AthenaArray<Real> &wr)
 {
-  for (int n=0; n<=NVAPOR; ++n)
+  MeshBlock *pmb = pmy_block_;
+  Real v[5], scale;
+  for (int n=0; n<NMASS; ++n) {
+    scale = 1.E-16;
 #pragma omp simd
+    for (int m = -2; m <= 2; ++m) scale += w(n,k,j,il+m);
     for (int i=il; i<=iu; ++i) {
-      Real scale = 0.;
-      for (int m = -2; m <= 2; ++m) scale += (w(n,k+m,j,i) + 1.E-16)/5.;
-      wl(n,i) = interp_weno5(w(n,k+2,j,i)/scale, w(n,k+1,j,i)/scale, w(n,k,j,i)/scale,
-                             w(n,k-1,j,i)/scale, w(n,k-2,j,i)/scale);
-      wl(n,i) *= scale;
-      wr(n,i) = interp_weno5(w(n,k-2,j,i)/scale, w(n,k-1,j,i)/scale, w(n,k,j,i)/scale,
-                             w(n,k+1,j,i)/scale, w(n,k+2,j,i)/scale);
-      wr(n,i) *= scale;
-    }
-
-  for (int n=NVAPOR+1; n<NMASS; ++n) 
 #pragma omp simd
-    for (int i=il; i<=iu; ++i) {
-      Real scale = 0.;
-      for (int m = -1; m <= 1; ++m) scale += (w(n,k+m,j,i) + 1.E-16)/3.;
-      wl(n,i) = interp_weno3(w(n,k+1,j,i)/scale, w(n,k,j,i)/scale, w(n,k-1,j,i)/scale);
-      wl(n,i) *= scale;
-      wr(n,i) = interp_weno3(w(n,k-1,j,i)/scale, w(n,k,j,i)/scale, w(n,k+1,j,i)/scale);
-      wr(n,i) *= scale;
+      for (int m = -2; m <= 2; ++m) v[m+2] = w(n,k+m,j,i)/(5.*scale);
+      // wl(k+1/2)
+      wl(n,i) = interp_weno5(v[4], v[3], v[2], v[1], v[0])*5.*scale;
+      // wr(k-1/2)
+      wr(n,i) = interp_weno5(v[0], v[1], v[2], v[3], v[4])*5.*scale;
+      scale += w(n,k,j,i+3) - w(n,k,j,i-2);
     }
+  }
 
-  for (int n=NMASS; n<NHYDRO; ++n) {
-    //if (n == IVZ || n == IPR)
+  int ng1 = 0, ng2 = 0;
+  if (pmb->pbval->block_bcs[inner_x2] != BoundaryFlag::block)
+    ng1 = NGHOST;
+  if (pmb->pbval->block_bcs[outer_x2] != BoundaryFlag::block)
+    ng2 = NGHOST;
+
+  if ((k < pmb->ks-1 + ng1) || (k > pmb->ke+1 - ng2)) {
+    for (int n=NMASS; n<NHYDRO; ++n) {
+      scale = 1.E-16;
+#pragma omp simd
+      for (int m = -2; m <= 2; ++m) scale += w(n,k,j,il+m);
+      for (int i=il; i<=iu; ++i) {
+#pragma omp simd
+        for (int m = -2; m <= 2; ++m) v[m+2] = w(n,k+m,j,i)/(5.*scale);
+        // wl(k+1/2)
+        wl(n,i) = interp_weno5(v[4], v[3], v[2], v[1], v[0])*5.*scale;
+        // wr(k-1/2)
+        wr(n,i) = interp_weno5(v[0], v[1], v[2], v[3], v[4])*5.*scale;
+        scale += w(n,k,j,i+3) - w(n,k,j,i-2);
+      }
+    }
+  } else {
+    for (int n=NMASS; n<NHYDRO; ++n) {
 #pragma omp simd
       for (int i=il; i<=iu; ++i) {
         wl(n,i) = interp_cp5(w(n,k+2,j,i),w(n,k+1,j,i),w(n,k,j,i),w(n,k-1,j,i),w(n,k-2,j,i));
         wr(n,i) = interp_cp5(w(n,k-2,j,i),w(n,k-1,j,i),w(n,k,j,i),w(n,k+1,j,i),w(n,k+2,j,i));
       }
-    /*else
-#pragma omp simd
-      for (int i=il; i<=iu; ++i) {
-        wl(n,i) = interp_bp5(w(n,k+3,j,i),w(n,k+2,j,i),w(n,k+1,j,i),
-                             w(n,k,j,i),w(n,k-1,j,i),w(n,k-2,j,i),-sign(w(IVZ,k,j,i)));
-        wr(n,i) = interp_bp5(w(n,k-3,j,i),w(n,k-2,j,i),w(n,k-1,j,i),
-                             w(n,k,j,i),w(n,k+1,j,i),w(n,k+2,j,i), sign(w(IVZ,k,j,i)));
-      }*/
+    }
   }
 
   return;
